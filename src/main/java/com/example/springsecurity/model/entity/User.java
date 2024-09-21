4ab75b2254cfa9182a974ffa457113e281c3b47d
lephuc230204 <lephuc04b3@gmail.com>
2024-09-21 11:10:26 +0700

    Update model User and Role

--- src/main/java/com/example/springsecurity/model/entity/User.java
+++ src/main/java/com/example/springsecurity/model/entity/User.java
@@ -1,19 +1,72 @@
 package com.example.springsecurity.model.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name ="Users")
-public class User {
+public class User implements UserDetails {
     @Id
-    Long id;
-    String name;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
 
+    private String username;
+    @Column(unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
 
+    @ManyToOne
+    @JoinColumn(name = "Role_ID", nullable = false)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getName() == null ? List.of() : List.of(new SimpleGrantedAuthority(role.getName()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {return true;}
+
+    public Long getId() {
+        return userId;
+    }
+
+    public void setId(Long id) {
+        this.userId = id;
+    }
 }
