d0d5ca59dae90844b2f78dec4e13cbe4a18c7e6c
lephuc230204 <lephuc04b3@gmail.com>
2024-09-22 18:29:50 +0700

    Update

--- src/main/java/com/example/springsecurity/controllers/AuthController.java
+++ src/main/java/com/example/springsecurity/controllers/AuthController.java
@@ -1,4 +1,27 @@
 package com.example.springsecurity.controllers;
 
+import com.example.springsecurity.model.payload.SignInForm;
+import com.example.springsecurity.model.payload.SignUpForm;
+import com.example.springsecurity.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody SignInForm form){
+        return ResponseEntity.ok(authService.login(form));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> login(@RequestBody  SignUpForm form){return ResponseEntity.ok(authService.register(form));}
+
+    @GetMapping("/refresh")
+    public ResponseEntity refreshToken(@RequestHeader("X-Refresh-Token") String refreshToken){return ResponseEntity.ok(authService.refreshJWT(refreshToken));}
 }
--- src/main/java/com/example/springsecurity/controllers/UserController.java
+++ src/main/java/com/example/springsecurity/controllers/UserController.java
@@ -1,4 +1,7 @@
 package com.example.springsecurity.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/user")
 public class UserController {
 }
