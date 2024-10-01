c96169f95802fb8a88479760e2f1cec2b0aa6c60
lephuc230204 <lephuc04b3@gmail.com>
2024-10-01 22:22:46 +0700

    Phuc update change properties 01/10

--- src/main/java/com/example/springsecurity/model/dto/AuthDto.java
+++ src/main/java/com/example/springsecurity/model/dto/AuthDto.java
@@ -1,25 +1,21 @@
 package com.example.springsecurity.model.dto;
 
 import com.example.springsecurity.model.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthDto {
     private Long id;
     private String token;
     private String refreshToken;
     private String status;
     private String result;
-
-    public AuthDto(Long id, String token, String refreshToken, String status, String result) {
-        this.id = id;
-        this.token = token;
-        this.refreshToken = refreshToken;
-        this.status = status;
-        this.result = result;
-    }
 
     public static AuthDto from(User user, String token, String refreshToken, String status, String result) {
         return AuthDto.builder()
