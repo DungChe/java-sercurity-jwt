9dc822e060373471865531947c62e00f17753bca
lephuc230204 <lephuc04b3@gmail.com>
2024-10-15 19:55:37 +0700

    Updated enum for field serviceType, Status

--- src/main/java/com/example/springsecurity/model/entity/Order.java
+++ src/main/java/com/example/springsecurity/model/entity/Order.java
@@ -2,6 +2,7 @@ package com.example.springsecurity.model.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,28 +11,29 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "Orders")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
+    private String title;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
-
+    private String orderNumber;
     private String designDetails;
-
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
-
-    private String serviceType; // "thiet ke", "bao tri", etc.
+    private String address;
+    private ServiceType serviceType; // DESIGN,MAINTENANCE,REPAIR etc.
     private LocalDate startDate;
     private LocalDate endDate;
-    private String status; // "peding , InProgress", "Completed, Canceled"
+    private Status status; // "peding , InProgress", "Completed, Canceled"
 
-//    public enum ServiceType{
-//        DESIGN,MAINTENANCE
-//    }
-//    public enum Status{
-//        PENDING, INPROGRESS, COMPLETED, CANCELED
-//    }
+    public enum ServiceType{
+        DESIGN,MAINTENANCE,REPAIR
+    }
+    public enum Status{
+        PENDING, INPROGRESS, COMPLETED, CANCELED
+    }
 }
