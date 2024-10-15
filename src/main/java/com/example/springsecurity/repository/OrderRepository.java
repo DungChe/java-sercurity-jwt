c86a2104143fee6ced0ebf6420c68c6b77a6af4d
lephuc230204 <lephuc04b3@gmail.com>
2024-10-15 19:53:40 +0700

    added findOrderIdAndUserId, findById

--- src/main/java/com/example/springsecurity/repository/OrderRepository.java
+++ src/main/java/com/example/springsecurity/repository/OrderRepository.java
@@ -3,11 +3,19 @@ package com.example.springsecurity.repository;
 import com.example.springsecurity.model.entity.Order;
 import com.example.springsecurity.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUser(User user);
 
-    boolean existsByUser(User user); // dùng khi dl user
+    boolean existsByUser(User user); // Kiểm tra sự tồn tại của order dựa trên user
+
+//    boolean existsCanceled ("CANCELED");
+    Optional<Order> findByOrderIdAndUserId(Long orderId, Long userId);
+
+    Optional<Order> findById(Long orderId); // Truy vấn theo orderId
 }
