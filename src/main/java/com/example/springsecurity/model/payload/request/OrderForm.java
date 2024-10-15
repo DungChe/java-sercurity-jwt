28c315d36f4b98b344c412eb2df76be8618d998e
lephuc230204 <lephuc04b3@gmail.com>
2024-10-15 19:54:01 +0700

    orderFrom to user Order

---
+++ src/main/java/com/example/springsecurity/model/payload/request/OrderForm.java
@@ -0,0 +1,27 @@
+package com.example.springsecurity.model.payload.request;
+
+import com.example.springsecurity.model.entity.Order;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class OrderForm {
+    @NotBlank
+    private String designDetails;
+    @NotBlank
+    private Order.ServiceType serviceType; // e.g., "Cleaning", "Maintenance", etc.
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate startDate;
+    @NotBlank
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate endDate;
+    @NotBlank
+    private String address;
+}
