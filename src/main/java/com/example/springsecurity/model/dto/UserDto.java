8a5dc14bf34366331cea9edeea8803f8e11c4f67
lephuc230204 <lephuc04b3@gmail.com>
2024-10-15 19:49:45 +0700

    deleted full name, add field phone, address

--- src/main/java/com/example/springsecurity/model/dto/UserDto.java
+++ src/main/java/com/example/springsecurity/model/dto/UserDto.java
@@ -1,12 +1,14 @@
 package com.example.springsecurity.model.dto;
 
 import com.example.springsecurity.model.entity.User;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -15,21 +17,28 @@ public class UserDto {
     private Long id;
     private String username;
     private String email;
-    private String fullName;
+    private String phone;
+    private String address;
     private String profilePicture;
     private String roleName;
     private LocalDate createdDate;
+    private LocalDate dob;
     private String status;
 
-    public static UserDto from(User user) {
+    public static UserDto to(User user) {
         return UserDto.builder()
                 .id(user.getId())
-                .fullName(user.getFullName())
                 .email(user.getEmail())
+                .username(user.getUsername())
+                .phone(user.getPhone())
+                .address(user.getAddress().toString())
                 .profilePicture(user.getProfilePicture())
                 .roleName(user.getRole() != null ? user.getRole().getName() : null)
                 .createdDate(user.getCreatedDate())
+                .dob(user.getDob())
                 .status(user.getStatus())
                 .build();
     }
+
+
 }
