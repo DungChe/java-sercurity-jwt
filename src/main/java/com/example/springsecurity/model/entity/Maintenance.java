865efdb359bf6c283727fcbfa7066672a4582a55
lephuc230204 <lephuc04b3@gmail.com>
2024-10-24 12:34:04 +0700

    Entity MainTainenace

---
+++ src/main/java/com/example/springsecurity/model/entity/Maintenance.java
@@ -0,0 +1,38 @@
+package com.example.springsecurity.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table( name = "maintence")
+public class Maintenance {
+    private String title;    // tieu de
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long maintenanceID;
+    private String price;
+    private String numberOrder;
+    private String serviceType;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private String constructionStaff;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate startDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate endDate;
+    private String content;     // noi dung
+
+}
