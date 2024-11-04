e00a5c3cf907400c630b6e78064cee5deef71f08
MR trum <vinhthanhnguyen12122004@gmail.com>
2024-11-04 14:19:36 +0700

    add function logout

--- src/main/java/com/example/springsecurity/service/AuthService.java
+++ src/main/java/com/example/springsecurity/service/AuthService.java
@@ -5,11 +5,13 @@ import com.example.springsecurity.model.payload.request.SignInForm;
 import com.example.springsecurity.model.payload.request.SignUpForm;
 import com.example.springsecurity.model.payload.response.ResponseData;
 import com.example.springsecurity.model.payload.response.ResponseError;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface AuthService {
     AuthDto login(SignInForm form);
     ResponseData<String> register(SignUpForm form);
     AuthDto refreshJWT(String refreshToken);
     String confirmUser(long userId, String otpCode);
-
+    ResponseData<String> logout(HttpServletRequest request, HttpServletResponse response);
 }
