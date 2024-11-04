2fe9b636464131cc2ab8ee2859b49f17c5a70f16
MR trum <vinhthanhnguyen12122004@gmail.com>
2024-11-04 14:21:12 +0700

    interacting db for Blacklisttoken

---
+++ src/main/java/com/example/springsecurity/repository/BlacklistTokenRepository.java
@@ -0,0 +1,11 @@
+package com.example.springsecurity.repository;
+
+import com.example.springsecurity.model.entity.BlacklistToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BlacklistTokenRepository extends JpaRepository<BlacklistToken, Long> {
+    Optional<BlacklistToken> findByToken(String token);
+    boolean existsByToken(String token);
+}
