c39874f0d1903bca8735b54fe4a18fb0de374bba
lephuc230204 <lephuc04b3@gmail.com>
2024-11-07 12:16:17 +0700

    add manament order inprogress

--- src/main/java/com/example/springsecurity/controllers/DesignStaffController.java
+++ src/main/java/com/example/springsecurity/controllers/DesignStaffController.java
@@ -1,18 +1,30 @@
 package com.example.springsecurity.controllers;
 
 import com.example.springsecurity.model.dto.DesignRecordDto;
+import com.example.springsecurity.model.dto.OrderDto;
 import com.example.springsecurity.model.payload.request.DesignRecordForm;
 import com.example.springsecurity.model.payload.response.ResponseData;
 import com.example.springsecurity.service.DesignRecordService;
+import com.example.springsecurity.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/designs")
 public class DesignStaffController {
     @Autowired
     private DesignRecordService designRecordService;
+    @Autowired
+    private OrderService orderService;
+
+    // XEM TẤT CẢ ĐƠN HÀNG CAN THIẾT KẾ BẢN VẼ
+    @GetMapping("/get-orders-inprogress")
+    public ResponseEntity<ResponseData<List<OrderDto>>> getAllOrderInProgress() {
+        return ResponseEntity.ok(orderService.getAllOrderInProgress());
+    }
 
     // TẠO HỒ SƠ THIẾT KẾ BẢN VẼ
     @PostMapping("/create/{customerId}")
@@ -26,6 +38,7 @@ public class DesignStaffController {
         return ResponseEntity.ok(designRecordService.updateDesignRecord(recordId, form));
     }
 
+    // XOA BẢN VẼ
     @DeleteMapping("/delete/{recordId}")
     public ResponseEntity<ResponseData<DesignRecordDto>> deleteRecord(@PathVariable Long recordId){
         return ResponseEntity.ok(designRecordService.deleteRecord(recordId));
