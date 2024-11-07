ae378cf2d56b7ad981eab41d000e4a0bbf463b7e
lephuc230204 <lephuc04b3@gmail.com>
2024-11-07 12:17:02 +0700

    status for order

--- src/main/java/com/example/springsecurity/model/payload/request/ChangeStatusOrderForm.java
+++ src/main/java/com/example/springsecurity/model/payload/request/ChangeStatusOrderForm.java
@@ -1,10 +1,9 @@
 package com.example.springsecurity.model.payload.request;
 
 
-import com.example.springsecurity.model.entity.Order;
 import lombok.Data;
 
 @Data
 public class ChangeStatusOrderForm {
-    private Order.Status status;
+    private String status;
 }
