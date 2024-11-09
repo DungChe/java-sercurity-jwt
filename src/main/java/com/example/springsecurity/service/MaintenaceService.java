012169af81fab19c76b540625e12419cb7139749
HuuDaihm <huudaiqng2020@gmail.com>
2024-11-09 18:55:15 +0700

    add method userGetMaintenance, getMaintenanceById

--- src/main/java/com/example/springsecurity/service/MaintenaceService.java
+++ src/main/java/com/example/springsecurity/service/MaintenaceService.java
@@ -5,13 +5,16 @@ import com.example.springsecurity.model.dto.OrderDto;
 import com.example.springsecurity.model.payload.request.MaintenanceForm;
 import com.example.springsecurity.model.payload.response.ResponseData;
 
+import java.security.Principal;
 import java.util.List;
 
 public interface MaintenaceService {
     ResponseData<MaintenanceDto> createMaintenance(MaintenanceForm form, Long orderId);
-    ResponseData<List<OrderDto>> getAll();
+    ResponseData<List<OrderDto>> getAllOrderMaintenance();
     // Chinh sua hon don bao tri
     ResponseData<MaintenanceDto> change(Long id, MaintenanceForm form);
-
-
+    // User Xem hoa don bao tri cua minh
+    ResponseData<List<MaintenanceDto>> userGetMaintenance(Principal principal);
+    // Consulting staff xem chi tiet 1 hoa don bao tri
+    ResponseData<MaintenanceDto> getMaintenanceById(Long id);
 }
