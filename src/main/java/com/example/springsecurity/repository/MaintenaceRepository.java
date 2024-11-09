0554be59ebdfcec0378fe28e4e344df5f050c731
HuuDaihm <huudaiqng2020@gmail.com>
2024-11-09 18:53:35 +0700

    add find User by ID

--- src/main/java/com/example/springsecurity/repository/MaintenaceRepository.java
+++ src/main/java/com/example/springsecurity/repository/MaintenaceRepository.java
@@ -3,10 +3,11 @@ package com.example.springsecurity.repository;
 import com.example.springsecurity.model.entity.Maintenance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MaintenaceRepository extends JpaRepository<Maintenance, Long> {
     Optional<Maintenance> findById( Long maintenaceId);
 
-
+    List<Maintenance> findByUserId(Long userId);
 }
